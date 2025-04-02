0afe496fc5eb8585ab4ddad4b2b6c600c3b91800
Israel Santana <is.wsie@gmail.com>
2025-04-02 16:13:44 -0400

    update main method to print new messages and add .gitignore for class files

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args)  {
+      System.out.println("I love pizza");
+      System.out.println("It's really good!");    }
 }
