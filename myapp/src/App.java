e177500a1a568339c10fcf50389ee6e0a087592b
Israel Santana <is.wsie@gmail.com>
2025-04-10 09:12:43 -0400

    Learned about while and do loops with Java

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -3,47 +3,34 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        // ! = (NOT) reverses boolean value of condition
         Scanner scanner = new Scanner(System.in);
+        String name = "";
 
-        System.out.println("You are playing a game! Press q or Q to quit");
-        String response = scanner.next();
-
-        if (!response.equals("q") && !response.equals("Q")) {
-            System.out.println("You are still playing the game *pew pew*");
-        } else {
-            System.out.println("You quit the game");
-
+        // while loop = executes a block of code as long as its's condition remains true
+        while (name.isBlank()) {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
         }
+
+        System.out.println("Hello " + name);
+        // It's good practice to close your scanner:
+        scanner.close();
+
+        // while loop with int ex:
+        int count = 10;
+        while (count > 0) {
+            System.out.println("Counting.... " + count);
+            count--;
+        }
+        System.out.print("The final count is: " + count);
+
+        /*
+         * do loop = executes block of code at least one and then it checks condition
+         * do {
+         * System.out.print("Enter your name: ");
+         * name = scanner.nextLine();
+         * } while (name.isBlank());
+         * System.out.println("Hello " + name);
+         */
     }
 }
-
-/*
- * Logical Operators: used to connect two or more expressions. *
- * 
- * && = (AND) both conditions must be true.
- * int temp = 15;
- * 
- * if (temp > 30) {
- * System.out.println("It is hot outside");
- * } else if (temp >= 20 && temp <= 30) {
- * System.out.println("It is warm outside");
- * } else {
- * System.out.println("It is cold outside");
- * }
- * 
- * || = (OR) either condition must be true.
- * 
- * Scanner scanner = new Scanner(System.in);
- * 
- * System.out.println("You are playing a game! Press q or Q to quit");
- * String response = scanner.next();
- * 
- * if (response.equals("q") || response.equals("Q")) {
- * System.out.println("You quit the game");
- * } else {
- * System.out.println("You are still playing the game *pew pew*");
- * }
- * 
- * 
- */
