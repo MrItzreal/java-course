db958616e55a019e30b08136a147faeedc372cae
Israel Santana <is.wsie@gmail.com>
2025-04-10 14:50:42 -0400

    Learned about nested loops in java

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
-        // for loop = executes a block of code a limited amount of times.
-        System.out.println("Lets count from 0 to 10!");
+        // nested loops = a loop inside a loop
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol = "";
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+        System.out.println("Enter # of rows: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter # of columns: ");
+        columns = scanner.nextInt();
+        System.out.println("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.println();
+            for (int j = 1; j <= columns; j++) {
+                System.out.print(symbol);
+            }
         }
-
-        System.out.println("Now count to 0 in increments of 2!");
-
-        for (int i = 10; i >= 0; i -= 2) {
-            System.out.println(i);
-        }
-
     }
 }
