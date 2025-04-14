eeca708549daed23d55284c57bab86b1b52c4ea3
Israel Santana <is.wsie@gmail.com>
2025-04-14 16:14:22 -0400

    Learned how 2D ArrayList work in Java

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,24 +1,42 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        // ArrayList = a resizable array.
-        // Elements can be added and removed after compilation phase.
-        // Stores reference data types such as strings.
+        // 2D ArrayList: a dynamic list of lists
+        // You can change the size of these lists during runtime
+        // To retrieve an element we use .get()
 
-        ArrayList<String> food = new ArrayList<String>();
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
 
-        food.add("Pizza");
-        food.add("Hamburger");
-        food.add("Pizza");
+        ArrayList<String> bakeryList = new ArrayList();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
 
-        // Useful ArrayList methods:
-        food.set(0, "Sushi");
-        food.remove(2);
-        // food.clear(); removes the elements.
+        // System.out.println(bakeryList.get(0));
 
-        for (int i = 0; i < food.size(); i++) {
-            System.out.println(food.get(i));
-        }
+        ArrayList<String> produceList = new ArrayList();
+        produceList.add("tomatoes");
+        produceList.add("zucchini");
+        produceList.add("peppers");
+
+        // System.out.println(produceList.get(0));
+
+        ArrayList<String> drinksList = new ArrayList();
+        drinksList.add("soda");
+        drinksList.add("coffee");
+
+        // System.out.println(drinksList.get(0));
+
+        // We are using a double array to print every list within the groceryList
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinksList);
+
+        // Gets one of the arrays
+        System.out.println(groceryList.get(1));
+
+        // Gets both the array and the specific element you want
+        System.out.println(groceryList.get(1).get(1));
     }
 }
