9a6d57ba5be6d12c9e86bba1125ce86e16e0bb2d
Israel Santana <is.wsie@gmail.com>
2025-04-16 16:30:24 -0400

    Learned about 'Overloaded methods'

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,43 +1,47 @@
 public class App {
     public static void main(String[] args) {
         /*
-         * method: a block of code that is executed
-         * whenever it is called upon.
+         * Overloaded methods: methods that share the same name but
+         * have different parameters.
+         * 
+         * With overloaded methods not only are the numbers of params
+         * taken into account but also the data types for each param.
+         * 
+         * // method name + parameters = method signature.
          */
 
-        int x = 3;
-        int y = 4;
-
-        int z = add(x, y);
-        System.out.println(z);
+        // int x = add(1, 2, 3, 4);
+        double x = add(1.0, 2.0, 3.0, 4.0);
+        System.out.println(x);
     }
 
-    static int add(int x, int y) {
-        int z = x + y;
-        return z;
+    static int add(int a, int b) {
+        System.out.println("This is overloaded method #1");
+        return a + b;
     }
 
+    static int add(int a, int b, int c) {
+        System.out.println("This is overloaded method #2");
+        return a + b + c;
+    }
+
+    static int add(int a, int b, int c, int d) {
+        System.out.println("This is overloaded method #3");
+        return a + b + c + d;
+    }
+
+    static double add(double a, double b) {
+        System.out.println("This is overloaded method #4");
+        return a + b;
+    }
+
+    static double add(double a, double b, double c) {
+        System.out.println("This is overloaded method #5");
+        return a + b + c;
+    }
+
+    static double add(double a, double b, double c, double d) {
+        System.out.println("This is overloaded method #6");
+        return a + b + c + d;
+    }
 }
-
-/*
- * public class App {
- * public static void main(String[] args) {
- * /*
- *
- * String name = "Izzy";
- * int age = 21;
- * 
- * hello(name, age);
- * }
- * 
- * // Do not confused an argument with a param.
- * // the "name" "age" below are params and as long as it
- * // matches the pattern of the above name, age the argument
- * // will print "Izzy" and "21":
- * static void hello(String name, int age) {
- * System.out.println("Hello " + name);
- * System.out.println("You are " + age + " years old");
- * 
- * }
- * }
- */
