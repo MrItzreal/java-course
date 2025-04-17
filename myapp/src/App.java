d1761fcb0ec5da91035a58f087ea2735f5b8601c
Israel Santana <is.wsie@gmail.com>
2025-04-17 11:52:02 -0400

    Learned about the 'constructor' method and the purpose of using 'this.'

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,26 +1,15 @@
 public class App {
     public static void main(String[] args) {
-        Car myCar1 = new Car();
-        Car myCar2 = new Car();
+        /*
+         * constructor = special method that is called when an object is
+         * 'instantiated' fancy word for 'created'.
+         */
+        Human human1 = new Human("Izzy", 49, 70);
+        Human human2 = new Human("Bro", 92, 30);
 
-        System.out.println(myCar1.make);
-        System.out.println(myCar1.model);
-
-        // myCar1.drive();
-        // myCar1.brake();
-
-        System.out.println();
-
-        System.out.println(myCar2.make);
-        System.out.println(myCar2.model);
-
+        System.out.println(human1.name);
+        System.out.println(human2.name);
+        human1.eat();
+        human2.drink();
     }
 }
-
-/*
- * Object Oriented Programming aka OOP.
- * 
- * object: an instance of a class that may contain attributes and methods.
- * example: (phone, desk, computer, coffee cup).
- * 
- */
