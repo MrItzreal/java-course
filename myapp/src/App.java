908539370af03959044ecc8fb70e0186650d4ad9
Israel Santana <is.wsie@gmail.com>
2025-04-16 20:53:01 -0400

    Learned about the printf() method, format specifier, conversion-character, width, precision and flags

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,47 +1,73 @@
 public class App {
     public static void main(String[] args) {
         /*
-         * Overloaded methods: methods that share the same name but
-         * have different parameters.
+         * printf(): an optional method to control, format, and display text
+         * to the console window. This is different from the "println" and "print".
+         * printf needs two arguments:
+         * a format string + (object/variable/value).
          * 
-         * With overloaded methods not only are the numbers of params
-         * taken into account but also the data types for each param.
-         * 
-         * // method name + parameters = method signature.
+         * NOTES:
+         * - %: format specifier: [flags] [precision] [width] [conversion-character].
+         * - Depending where I add the %d the argument within the format string will
+         * move.
+         * - %d the 'd' is a conversion character this one is for ints and decimals.
+         * There are other as well.
          */
 
-        // int x = add(1, 2, 3, 4);
-        double x = add(1.0, 2.0, 3.0, 4.0);
-        System.out.println(x);
-    }
+        // System.out.printf("This is a format string %d", 123);
+        // System.out.printf("%d This is a format string", 123);
+        // System.out.printf("This is a %d format string", 123);
 
-    static int add(int a, int b) {
-        System.out.println("This is overloaded method #1");
-        return a + b;
-    }
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Izzy";
+        int myInt = 50;
+        double myDouble = 1000;
 
-    static int add(int a, int b, int c) {
-        System.out.println("This is overloaded method #2");
-        return a + b + c;
-    }
+        /*
+         * [conversion-character]:
+         * b: booleans.
+         * c: characters.
+         * s: strings.
+         * d: intergers.
+         * f: floats or doubles.
+         */
 
-    static int add(int a, int b, int c, int d) {
-        System.out.println("This is overloaded method #3");
-        return a + b + c + d;
-    }
+        // System.out.printf("%b ", myBoolean);
+        // System.out.printf("%c ", myChar);
+        // System.out.printf("%s ", myString);
+        // System.out.printf("%d ", myInt);
+        // System.out.printf("%f ", myDouble);
 
-    static double add(double a, double b) {
-        System.out.println("This is overloaded method #4");
-        return a + b;
-    }
+        /*
+         * [width]: minimum number of characters to be written as output. The
+         * number in this case '10' is the amount of space between the string and
+         * value. If the number is negative like '-10' then the spaces are on the
+         * opposite (right side).
+         */
+        // System.out.printf("Hello %10s", myString);
+        // System.out.printf("Hello %-10s", myString);
 
-    static double add(double a, double b, double c) {
-        System.out.println("This is overloaded method #5");
-        return a + b + c;
-    }
+        /*
+         * [precision]: sets number of digits of precision when outputting
+         * floating-point values. For instance, '.2' will limit the amount of
+         * digits that will appear after the decimal portion.
+         */
+        // System.out.printf("You have this much money %.2f", myDouble);
 
-    static double add(double a, double b, double c, double d) {
-        System.out.println("This is overloaded method #6");
-        return a + b + c + d;
+        /*
+         * [flags]: adds an effect to output based on the flag
+         * added to format specifier.
+         * 
+         * - : left-justify
+         * + : output a plus (+) or minus (-) sign for a numeric value.
+         * 0 : numeric values are zero-padded.
+         * , : comma grouping separator if numbers > 1000.
+         */
+        // System.out.printf("You have this much money %20f", myDouble);
+        // System.out.printf("You have this much money %+f", myDouble);
+        // System.out.printf("You have this much money %020f", myDouble);
+        // System.out.printf("You have this much money %,f", myDouble);
+
     }
 }
