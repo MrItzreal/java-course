aaf2f0e4fb336c3aa55c6051be435c035c4e44b0
Israel Santana <is.wsie@gmail.com>
2025-04-18 14:56:06 -0400

    Learned to send objects as arguments.

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,32 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        /*
-         * The numbers within the array is the size of the array if
-         * you are not planning to assign a size right away.
-         * 
-         * data type/name/size of array:
-         * int[] numbers = new int[3];
-         * char[] characters = new char[4];
-         * String[] strings = new String[5];
-         */
 
-        /*
-         * This declares our array + adding its size
-         * Food[] refrigerator = new Food[3];
-         */
-        Food food1 = new Food("pizza");
-        Food food2 = new Food("hamburger");
-        Food food3 = new Food("hotdog");
-        
-        // refrigerator[0] = food1;
-        // refrigerator[1] = food2;
-        // refrigerator[2] = food3;
+        Garage garage = new Garage();
+        Car car1 = new Car("BWM");
+        Car car2 = new Car("Nissan");
 
-        // This immediately declares and adds the size
-        Food[] refrigerator = { food1, food2, food3 };
+        garage.park(car1);
+        garage.park(car2);
 
-        System.out.println(refrigerator[0].name);
-        System.out.println(refrigerator[1].name);
-        System.out.println(refrigerator[2].name);
     }
 }
--- myapp/src/Car.java
+++ myapp/src/Car.java
@@ -1,25 +1,9 @@
 public class Car {
 
-  String make = "Nissan";
-  String model = "Rogue";
-  int year = 2024;
-  String color = "White";
-  double price = 40000.00;
+  String name;
 
-  public String toString() {
-    // You can write this like this:
-    // String myString = make + "\n" + model + "\n" + color + "\n" + year;
-
-    // You can also write it like this:
-    return make + "\n" + model + "\n" + color + "\n" + year;
+  Car(String name) {
+    this.name = name;
   }
 
-  // void examples:
-  void drive() {
-    System.out.println("You drive the car");
-  }
-
-  void brake() {
-    System.out.println("You stepped on the brakes");
-  }
 }
