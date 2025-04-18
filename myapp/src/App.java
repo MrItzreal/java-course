3198e4bc7af2b2098c06bbec2a243363ca28759e
Israel Santana <is.wsie@gmail.com>
2025-04-18 13:48:29 -0400

    Got to work with array of objects

--- myapp/src/App.java
+++ myapp/src/App.java
@@ -1,23 +1,32 @@
 public class App {
     public static void main(String[] args) {
         /*
-         * toString() = special method that all objects inherit,
-         * that returns a string that "textually represents" an object.
+         * The numbers within the array is the size of the array if
+         * you are not planning to assign a size right away.
          * 
-         * NOTES:
-         * - Can be used both implicitly and explicitly.
-         * - You can override the toString method to return a
-         * a string representation of all of the atributes of an object.
+         * data type/name/size of array:
+         * int[] numbers = new int[3];
+         * char[] characters = new char[4];
+         * String[] strings = new String[5];
          */
 
-        Car car = new Car();
+        /*
+         * This declares our array + adding its size
+         * Food[] refrigerator = new Food[3];
+         */
+        Food food1 = new Food("pizza");
+        Food food2 = new Food("hamburger");
+        Food food3 = new Food("hotdog");
+        
+        // refrigerator[0] = food1;
+        // refrigerator[1] = food2;
+        // refrigerator[2] = food3;
 
-        // Explicit use of the toString method.
-        System.out.println(car.toString());
+        // This immediately declares and adds the size
+        Food[] refrigerator = { food1, food2, food3 };
 
-        System.out.println("===OR====");
-
-        // implicit use of the toString method.
-        System.out.println(car);
+        System.out.println(refrigerator[0].name);
+        System.out.println(refrigerator[1].name);
+        System.out.println(refrigerator[2].name);
     }
 }
