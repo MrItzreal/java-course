fbafd617f59650a16b9041ccf7ee9851fd520707
Israel Santana <is.wsie@gmail.com>
2025-04-21 15:49:28 -0400

    Learned about the concept of "super" and its usage in subclasses and superclasses (aka parent class).

---
+++ myapp/src/super_keyword/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    /*
+     * super = keyword refers to the superclass (parent)
+     * of an object very similar to the "this" keyword.
+     */
+
+    Hero hero1 = new Hero("Batman", 42, "money");
+
+    Hero hero2 = new Hero("Superman", 44, "fly");
+
+
+    System.out.println(hero2);
+  }
+}
