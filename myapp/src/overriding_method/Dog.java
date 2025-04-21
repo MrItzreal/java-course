e260407bf8e9012a75fbc5e23a6cc81b2264abd2
Israel Santana <is.wsie@gmail.com>
2025-04-21 13:42:16 -0400

    Learned about the method overriding concept

---
+++ myapp/src/overriding_method/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal {
+
+  /*
+   * 'Override': Indicates that a method *declaration is intended to override a method
+   */
+  
+  @Override
+  void speak() {
+    System.out.println("The Dog barks!");
+  }
+}
