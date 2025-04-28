4e54ae0d1a6294eb30c67b66f4f6fa3481b33b28
MrItzreal <is.wsie@gmail.com>
2025-04-28 18:25:42 -0400

    Learned about 'KeyListeners'

---
+++ myapp/src/key_listener/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+
+    /*
+     * keyTyped = Invoked when a key is typed. Uses KeyChar,
+     * char output.
+     * 
+     * keyPressed = Invoked when a physical key is pressed down.
+     * Uses KeyCode, int output.
+     * 
+     * keyReleased = called whenever a button is released.
+     */
+
+    new MyFrame();
+  }
+}
